6114f217784f8e608a99273d90c3ab27e238cae1
Alejandrochambi1 <alejandrochambi499@gmail.com>
2025-11-21 00:43:38 -0400

    first commit

---
+++ Entidades/Articulo.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace simple.Entidades
+{
+    public class Articulo
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public  string codigo { get; set; }
+    }
+}
